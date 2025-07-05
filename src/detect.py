c0654fcc8b8a2f9e007b28b9b204fd6054767e25
Mher Ghazaryan <mher.ghazaryan5@gmail.com>
2025-07-05 18:14:36 +0400

    Collect frame info before rendering

--- src/detect.py
+++ src/detect.py
@@ -1,29 +1,46 @@
 import argparse
+import cv2
 import imutils
 import numpy as np
 import os
-import cv2
 import torch
 
-from ultralytics import YOLO
-from typing import Optional, Tuple
 from deepface import DeepFace
+from typing import Optional, Tuple
+from ultralytics import YOLO
 
-writer = None
-slow_down_ranges = []  # Initialize list to track slow-down ranges
+
+class BoxInfo:
+	def __init__(self, x1, y1, width, height, confidence):
+		self.x1 = x1
+		self.y1 = y1
+		self.width = width
+		self.height = height
+		self.confidence = confidence
+
+
+class FrameInfo:
+	def __init__(self, original_frame):
+		self.original_frame = original_frame
+		self.modified_frame = self.original_frame
+		self.is_slowmo = False
+		self.shoplifting_boxes = []
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+max_confidence_person_info = None
 
-def load_yolov8_model(weights_path):
+
+def load_yolo_model(weights_path):
 	if not os.path.exists(weights_path):
 		raise FileNotFoundError(f"model weights not found at: {weights_path}")
 	result = YOLO(weights_path).to(device)
 	return result
 
 
-shoplifting_detection_model = load_yolov8_model("../data/shoplifting.pt")
-person_info_model = load_yolov8_model("../data/yolov8n_person_detection.pt")
+shoplifting_detection_model = load_yolo_model("../data/shoplifting.pt")
+person_info_model = load_yolo_model("../data/yolov8n_person_detection.pt")
 
 
 def create_parser():
@@ -42,7 +59,60 @@ def load_video(path):
 	return result
 
 
+def create_video(path, fps, frame_shape):
+	fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+	result = cv2.VideoWriter(path, fourcc, fps, (frame_shape[1], frame_shape[0]), True)
+	return result
+
+
+def write_video(output_video, frame_infos):
+	for frame_info in frame_infos:
+		for box_info in frame_info.shoplifting_boxes:
+			cv2.rectangle(frame_info.modified_frame, (box_info.x1, box_info.y1),
+			              (box_info.x1 + box_info.width, box_info.y1 + box_info.height), (0, 0, 255), 2)
+		center_x = int(box_info.x1 + box_info.width / 2)
+		cv2.circle(frame_info.modified_frame, (center_x, box_info.y1), 6, (0, 255, 255), -1)
+		confidence_text = f"{np.round(box_info.confidence * 100, 2)}%"
+		cv2.putText(frame_info.modified_frame, confidence_text, (box_info.x1 + 10, box_info.y1 - 10),
+		            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+		            (0, 0, 255), 2)
+
+		if max_confidence_person_info is not None:
+			cv2.putText(frame_info.modified_frame,
+			            "Suspect",
+			            (30, 30),
+			            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+			            (0, 0, 255), 2)
+			cv2.putText(frame_info.modified_frame,
+			            f"Age {max_confidence_person_info[1]}",
+			            (60, 60),
+			            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+			            (0, 0, 255), 2)
+			cv2.putText(frame_info.modified_frame,
+			            f"Gender {max_confidence_person_info[2]}",
+			            (60, 90),
+			            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+			            (0, 0, 255), 2)
+			cv2.putText(frame_info.modified_frame,
+			            f"Race = {max_confidence_person_info[3]}",
+			            (60, 120),
+			            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+			            (0, 0, 255), 2)
+
+		output_video.write(frame_info.modified_frame)
+		if frame_info.is_slowmo:
+			for _ in range(9):
+				output_video.write(frame_info.modified_frame)
+
+		cv2.imshow("Generating output...", frame_info.modified_frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	cv2.destroyWindow("Generating output...")
+
+
 def detect_person_info(croped_frame: np.ndarray) -> Optional[Tuple[float, int, str, str]]:
+	global max_confidence_person_info
+
 	results = person_info_model(croped_frame, classes=[0])
 
 	for result in results:
@@ -50,26 +120,13 @@ def detect_person_info(croped_frame: np.ndarray) -> Optional[Tuple[float, int, s
 
 		for box in boxes:
 			x1, y1, x2, y2 = map(int, box.xyxy[0])
-			conf = float(box.conf[0])
+			confidence = float(box.conf[0])
 
 			try:
 				analysis = DeepFace.analyze(croped_frame, actions=['age', 'gender', 'race'], enforce_detection=True)[0]
-				# analysis = DeepFace.analyze(
-				#    img_path=croped_frame,
-				#    actions=['age', 'gender', 'race'],
-				#    models={
-				#        'age': age_model,
-				#        'gender': gender_model,
-				#        'race': race_model
-				#    },
-				#    enforce_detection=True
-				# )[0]
-				# conf_percent = round(conf * 100, 1)
-				# age = analysis["age"]
-				# gender = analysis["dominant_gender"]
-				# race = analysis["dominant_race"]
-
-				return (conf, analysis["age"], analysis["dominant_gender"], analysis["dominant_race"])
+				person_info = confidence, analysis["age"], analysis["dominant_gender"], analysis["dominant_race"]
+				if max_confidence_person_info is None or max_confidence_person_info[0] < confidence:
+					max_confidence_person_info = person_info
 
 			except Exception as e:
 				print(e)
@@ -78,111 +135,49 @@ def detect_person_info(croped_frame: np.ndarray) -> Optional[Tuple[float, int, s
 	return None
 
 
-def draw_shoplifting_box(frame, x, y, w, h, confidence, person_infos):
-	cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
-	center_x = int(x + w / 2)
-	cv2.circle(frame, (center_x, y), 6, (0, 0, 255), -1)
-	conf_text = f"{np.round(confidence * 100, 2)}%"
-	cv2.putText(frame, conf_text, (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
-	person_info = detect_person_info(frame[y: y + h, x: x + w])
-	if person_info:
-		person_infos.append(person_info)
+def process_shoplifting_box(frame, x, y, w, h, confidence, frame_info):
+	box = BoxInfo(x, y, w, h, confidence)
+	box.person_info = detect_person_info(frame[y: y + h, x: x + w])
+	frame_info.shoplifting_boxes.append(box)
 
 
-def draw_shoplifting_prediction(prediction, frame, person_infos, frame_count):
-	global slow_down_ranges
-	status = False
-
+def process_shoplifting_prediction(prediction, frame, frame_info):
 	xywh = np.array(prediction.boxes.xywh.cpu()).astype("int32")
 	xyxy = np.array(prediction.boxes.xyxy.cpu()).astype("int32")
 	cc_data = np.array(prediction.boxes.data.cpu())
 
 	for (x1, y1, _, _), (_, _, w, h), (_, _, _, _, confidence, classification) in zip(xyxy, xywh, cc_data):
 		if classification == 1 and confidence > 0.5:
-			status = True
-			draw_shoplifting_box(frame, x1, y1, w, h, confidence, person_infos)
-
-	if status:
-		start = max(0, frame_count - 70)
-		end = frame_count + 25
-		new_range = (start, end)
-		if should_add_range(new_range, slow_down_ranges):
-			slow_down_ranges.append(new_range)
-			slow_down_ranges = merge_ranges(slow_down_ranges)
+			process_shoplifting_box(frame, x1, y1, w, h, confidence, frame_info)
 
 
-def merge_ranges(ranges):
-	if not ranges:
-		return []
-	ranges.sort()
-	merged = [ranges[0]]
-	for current in ranges[1:]:
-		prev_start, prev_end = merged[-1]
-		curr_start, curr_end = current
-		if curr_start <= prev_end:
-			merged[-1] = (prev_start, max(prev_end, curr_end))
-		else:
-			merged.append(current)
-	return merged
-
-
-def should_add_range(new_range, existing_ranges):
-	new_start, new_end = new_range
-	for start, end in existing_ranges:
-		if not (new_end < start or new_start > end):  # Overlaps
-			return False
-	return True
-
-
-def draw_shoplifting_predictions(predictions, frame, person_infos, frame_count):
-	for prediction in predictions:
-		if prediction.boxes:
-			draw_shoplifting_prediction(prediction, frame, person_infos, frame_count)
-
-
-def process_frame(frame, person_infos, frame_count):
-	shoplifting_predictions = shoplifting_detection_model.predict(frame)
-	draw_shoplifting_predictions(shoplifting_predictions, frame, person_infos, frame_count)
+def process_frame(frame, frame_info):
+	shoplifting_prediction = shoplifting_detection_model.predict(frame)[0]
+	if shoplifting_prediction.boxes:
+		process_shoplifting_prediction(shoplifting_prediction, frame, frame_info)
 
 	return frame
 
 
-def setup_video_writer(frame: np.ndarray, output_path: str) -> None:
-	global writer
-	if output_path and writer is None:
-		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = cv2.VideoWriter(output_path, fourcc, 25,
-		                         (frame.shape[1], frame.shape[0]), True)
-
-
-def process_video(video, out_path):
-	person_infos = []
-	frame_count = 0
-	global slow_down_ranges, writer
-	slow_down_ranges = []  # Initialize list to track slow-down ranges
+def process_video(video):
+	result = []
 
 	while True:
 		has_frame, frame = video.read()
 		if not has_frame:
 			break
 		frame = imutils.resize(frame, width=800)
-		frame_count += 1
-		frame = process_frame(frame, person_infos, frame_count)
+		frame_info = FrameInfo(frame)
+		frame = process_frame(frame, frame_info)
+		frame_info.modified_frame = frame
 
-		setup_video_writer(frame, out_path)
-		if writer is not None:
-			# Write frames to output video with slow-down effect
-			slow_down_active = any(start <= frame_count <= end for start, end in slow_down_ranges)
-			if slow_down_active:
-				for _ in range(10):  # Write the same frame multiple times for slow-down
-					writer.write(frame)
-			else:
-				writer.write(frame)
-
-		cv2.imshow("Shoplifting detection", frame)
+		result.append(frame_info)
+		cv2.imshow("Processing...", frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
-	print(max(person_infos, key=lambda x: x[0]))
+	cv2.destroyWindow("Processing...")
+
+	return result
 
 
 def main():
@@ -190,7 +185,15 @@ def main():
 	args = parser.parse_args()
 
 	video = load_video(args.input)
-	process_video(video, args.output)
+	fps = video.get(cv2.CAP_PROP_FPS)
+
+	frame_infos = process_video(video)
+	if not frame_infos:
+		raise ValueError("no frames found")
+
+	output_video = create_video(args.output, fps,
+	                            (frame_infos[0].modified_frame.shape[0], frame_infos[0].modified_frame.shape[1]))
+	write_video(output_video, frame_infos)
 
 
 if __name__ == "__main__":
